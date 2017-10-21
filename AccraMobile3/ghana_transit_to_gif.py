4c844192af346a512fbbea70fb0b3ee970cb21df
Pascal Rhod <pascal.rhod@canaltp.fr>
2017-10-22 01:30:14 +0200

    Accra - generation du GIF avec les lignes

--- AccraMobile3/ghana_transit_to_gif.py
+++ AccraMobile3/ghana_transit_to_gif.py
@@ -1,16 +1,24 @@
-
 # coding: utf-8
 
-# ## Export des données Transport du Ghana
+# Export des données Transport du Ghana
 
-# In[1]:
-
+#nécessite phantomjs (https://gist.github.com/julionc/7476620) et selenium (sudo pip3 install selenium)
 
 import os
 import requests
 
+import datetime
+import osmium
+import pandas as pd
+from shapely.geometry import LineString, MultiLineString
+from shapely.ops import cascaded_union
+import ghana_transit_to_gif_handlers
+
+
+
 url_source = 'http://download.geofabrik.de/africa/ghana.osh.pbf'
 dest_file = './ghana.osh.pbf'
+
 if not os.path.isfile(dest_file):
     print("Downloading {}".format(dest_file))
     with open(dest_file, "wb") as file:
@@ -21,64 +29,18 @@ else:
     print("File {} already exists".format(dest_file))
 
 
-
-# In[2]:
-
-
-import datetime
-import osmium
-import pandas as pd
-import shapely.wkt as wktlib
-import shapely.wkb as wkblib
-
-wkbfab = osmium.geom.WKBFactory()
-
-class StopsHandler(osmium.SimpleHandler):
-    def __init__(self):
-        osmium.SimpleHandler.__init__(self)
-        self.start_date = datetime.datetime.strptime("01/07/2017", "%d/%m/%Y").replace(tzinfo=datetime.timezone.utc)
-
-    def node(self, n):
-        #on prend public_transport = platform et highway=bus_stop
-
-        if n.timestamp < self.start_date:
-            pass
-        elif ('public_transport' in n.tags and n.tags['public_transport'] == 'platform') and             ('highway' in n.tags and n.tags['highway'] == 'bus_stop') :
-            #print(dir(n.location))
-            wkb = wkbfab.create_point(n.location)
-            point = wkblib.loads(wkb, hex=True)
-            name = ""
-            public_transport = ""
-            highway = ""
-            if "name" in n.tags:
-                name = n.tags["name"]
-            if "public_transport" in n.tags:
-                public_transport = n.tags["public_transport"]
-            if "highway" in n.tags:
-                highway = n.tags["highway"]
-
-            if not n.id in stops:
-                stops[n.id] = {}
-                stops[n.id]["version"] = []
-                stops[n.id]["date"] = []
-                stops[n.id]["name"] = []
-                stops[n.id]["geometry"] = []
-            stops[n.id]["version"].append(n.version)
-            stops[n.id]["date"].append(n.timestamp)
-            stops[n.id]["name"].append(name)
-            stops[n.id]["geometry"].append(point)
-
+#===============================================
+#              Load Stops
 stops_file = './stops.csv'
-stops = {}
-stops_tmp = []
+stops = []
 if not os.path.isfile(stops_file):
     print("Le fichier {} n'existe pas, lancement du traitement".format(stops_file))
-    stops_handler = StopsHandler()
+    stops_handler = ghana_transit_to_gif_handlers.StopsHandler()
     stops_handler.apply_file(dest_file)
 
-    #on charge dans stops_tmp les données finales, avec la 1ère date de modification après le 1er juillet 2017
+    #on charge dans stops les données finales, avec la 1ère date de modification après le 1er juillet 2017
     #et le dernier nom et la dernière position connus
-    for k,v in stops.items():
+    for k,v in stops_handler.stops.items():
         min_version = min(v["version"])
         min_index = v["version"].index(min_version)
         max_version = max(v["version"])
@@ -90,69 +52,25 @@ if not os.path.isfile(stops_file):
              "last_name": v["name"][max_index],
              "last_geometry": v["geometry"][max_index]
             }
-        stops_tmp.append(s)
+        stops.append(s)
 
-    pd.DataFrame.from_dict(stops_tmp).to_csv('./stops.csv')
+    pd.DataFrame.from_dict(stops).to_csv('./stops.csv')
 else:
     print("Le fichier {} existe, chargement depuis le fichier".format(stops_file))
-    stops_tmp = pd.read_csv(stops_file).to_dict(orient="records")
-print('fin de chargement des stops : {:d}'.format(len(stops_tmp)))
+    stops = pd.read_csv(stops_file).to_dict(orient="records")
+print('fin de chargement des stops : {:d}'.format(len(stops)))
 
-# In[3]:
-
-
-import datetime
-import osmium
-import pandas as pd
-
-class RoutesHandler(osmium.SimpleHandler):
-    def __init__(self):
-        osmium.SimpleHandler.__init__(self)
-        self.start_date = datetime.datetime.strptime("01/07/2017", "%d/%m/%Y").replace(tzinfo=datetime.timezone.utc)
-
-    def relation(self, r):
-        #on prend type=route et route=bus
-        if r.timestamp < self.start_date:
-            pass
-        elif ('type' in r.tags and r.tags['type'] == 'route') and ('route' in r.tags and r.tags['route'] == 'bus') :
-            name = ""
-            ref = ""
-            r_type = ""
-            route = ""
-            if "ref" in r.tags:
-                ref = r.tags["ref"]
-            if "name" in r.tags:
-                name = r.tags["name"]
-            ways = []
-            for rm in r.members:
-                if not rm.type == 'w': continue
-                ways.append(rm.ref)
-
-            if not r.id in routes:
-                routes[r.id] = {}
-                routes[r.id]["version"] = []
-                routes[r.id]["date"] = []
-                routes[r.id]["ref"] = []
-                routes[r.id]["name"] = []
-                routes[r.id]["ways"] = []
-
-            routes[r.id]["version"].append(r.version)
-            routes[r.id]["date"].append(r.timestamp)
-            routes[r.id]["ref"].append(ref)
-            routes[r.id]["name"].append(name)
-            #ways est un tableau qui contient, pour chaque version, un tableau de ways
-            routes[r.id]["ways"].append(ways)
-
+#===============================================
+#              Load Relations
 routes_file1 = "./routes.csv"
-routes_tmp = []
+routes = []
 routes_all_ways = []
 if not os.path.isfile(routes_file1):
     print("Le fichier {} n'existe pas, lancement de la lecture des relations".format(routes_file1))
-    routes = {}
-    routes_handler = RoutesHandler()
+    routes_handler = ghana_transit_to_gif_handlers.RelationHandler()
     routes_handler.apply_file(dest_file)
 
-    for k,v in routes.items():
+    for k,v in routes_handler.routes.items():
         min_version = min(v["version"])
         min_index = v["version"].index(min_version)
         max_version = max(v["version"])
@@ -166,125 +84,60 @@ if not os.path.isfile(routes_file1):
              "last_ways": v["ways"][max_index]
             }
         routes_all_ways.extend(r["last_ways"])
-        routes_tmp.append(r)
+        routes.append(r)
 
     #il faut ensuite charger les dernières versions des ways utilisés par les relations pour avoir la géometrie
-    pd_routes = pd.DataFrame.from_dict(routes_tmp)
+    pd_routes = pd.DataFrame.from_dict(routes)
     pd_routes.to_csv('./routes.csv')
 else:
     print("Le fichier {} existe, chargement depuis le fichier".format(routes_file1))
-    routes_tmp = pd.read_csv(routes_file1).to_dict(orient="records")
-    for r in routes_tmp:
-        #liste stockée comme chaine de caractères => on la parse à la main (plus simple)
-        ways = r["last_ways"][1:-1]
-        ways = ways.split(",")
-        ways = [int(i) for i in ways if i]
-        r["last_ways"] = ways
-        routes_all_ways.extend(ways)
-routes = routes_tmp
+routes = pd.read_csv(routes_file1, parse_dates=["creation_date"]).to_dict(orient="records")
+for r in routes:
+    #liste stockée comme chaine de caractères => on la parse à la main (plus simple)
+    ways = r["last_ways"][1:-1]
+    ways = ways.split(",")
+    ways = [int(i) for i in ways if i]
+    r["last_ways"] = ways
+    routes_all_ways.extend(ways)
 print('fin de chargement des relations : {:d} (dont {:d} références de ways)'.format(len(routes), len(routes_all_ways)))
-# In[ ]:
 
-import osmium
-from copy import deepcopy
-import shapely.wkt as wktlib
-import shapely.wkb as wkblib
-wkbfab = osmium.geom.WKBFactory()
 
-class RoutesHandler2(osmium.SimpleHandler):
-    def __init__(self):
-        osmium.SimpleHandler.__init__(self)
-        self.print_step = 1000000
-        self.last_print_id = -1
-        self.starttime = datetime.datetime.now()
-
-    def way(self, w):
-        if w.id > self.last_print_id + self.print_step:
-            print("reading way id = {:d} (actually {:d} ways kept)".format(w.id, len(ways)))
-            print(datetime.datetime.now() - self.starttime)
-            self.last_print_id = w.id
-        if w.id in routes_all_ways:
-            if not w.id in ways_all_nodes:
-                ways[w.id] = {}
-                ways[w.id]["object_id"] = w.id
-                ways[w.id]["version"] = ""
-                ways[w.id]["date"] = ""
-                ways[w.id]["nodes_ref"] = ""
-                #ways[w.id]["geom"] = []
-            if not ways[w.id]["version"] or ways[w.id]["version"] < w.version:
-                ways[w.id]["version"] = w.version
-                ways[w.id]["date"] = w.timestamp
-                ways[w.id]["nodes_ref"] = [n.ref for n in w.nodes]
-                ways_all_nodes.extend(ways[w.id]["nodes_ref"])
-
-                    # try:
-                    #    wkb = wkbfab.create_linestring(w.nodes)
-                    #    geom = wkblib.loads(wkb, hex=True)
-                    # except:
-                    #    print("create_linestring error with way {:d}".format(w.id))
-                    #    geom = ''
-                    # ways[w.id]["geom"] = geom
-
+#===============================================
+#              Load Ways
 ways_file = "./ways.csv"
-ways = {}
 ways_all_nodes = []
 routes_all_ways = set(routes_all_ways) #utilisation d'un set pour sacrément accélerer la vérif de présence d'un item /!\
 if not os.path.isfile(ways_file):
     print("Le fichier {} n'existe pas, lancement de la lecture des ways".format(ways_file))
-    routes_handler2 = RoutesHandler2()
-    routes_handler2.apply_file(dest_file)
+    way_handler = ghana_transit_to_gif_handlers.WayHandler(routes_all_ways)
+    way_handler.apply_file(dest_file)
     print("Ecriture du fichier {}".format(ways_file))
-    ways_tmp = [w for w in ways.values()]
-    pd.DataFrame.from_dict(ways_tmp).to_csv(ways_file)
+    ways = [w for w in way_handler.ways.values()]
+    pd.DataFrame.from_dict(ways).to_csv(ways_file)
 else:
     print("Le fichier {} existe, chargement depuis le fichier".format(ways_file))
-    ways = pd.read_csv(ways_file).to_dict(orient="records")
-    for w in ways:
-        points = w["nodes_ref"][1:-1]
-        points = points.split(",")
-        points = [int(i) for i in points if i]
-        ways_all_nodes.extend(points)
-        w["nodes_ref"] = points
+ways = pd.read_csv(ways_file).to_dict(orient="records")
+for w in ways:
+    points = w["nodes_ref"][1:-1]
+    points = points.split(",")
+    points = [int(i) for i in points if i]
+    ways_all_nodes.extend(points)
+    w["nodes_ref"] = points
 print("Chargement des ways terminé : {:d}".format(len(ways)))
 print("nombre de refs de node : {:d}".format(len(ways_all_nodes)))
 
-class RoutesHandler3(osmium.SimpleHandler):
-    def __init__(self):
-        osmium.SimpleHandler.__init__(self)
 
-    def node(self, n):
-        if n.id in ways_all_nodes:
-            if not n.id in nodes:
-                nodes[n.id] = {}
-                nodes[n.id]["object_id"] = n.id
-                nodes[n.id]["version"] = ""
-                nodes[n.id]["lat"] = ""
-                nodes[n.id]["lon"] = ""
-                nodes[n.id]["valid"] = ""
-            if not nodes[n.id]["version"] or nodes[n.id]["version"] < n.version:
-                nodes[n.id]["version"] = n.version
-                try:
-                    if n.location.valid:
-                        nodes[n.id]["lat"] = n.location.lat
-                        nodes[n.id]["lon"] = n.location.lon
-                    else:
-                        print("point invalide : {:d}".format(n.id))
-                except Exception as e:
-                    print("exception sur le node {:d}".format(nodes[n.id]["object_id"]))
-                    #raise
-            # if len(nodes) % 1000 == 0:
-            #     print(len(nodes))
-
+#===============================================
+#              Load Nodes
 #pas de sauvegarde fichier pour les nodes, le chargement est rapide
-nodes = {}
 ways_all_nodes = set(ways_all_nodes)
-routes_handler3 = RoutesHandler3()
+routes_handler3 = ghana_transit_to_gif_handlers.NodeHandler(ways_all_nodes)
 routes_handler3.apply_file(dest_file)
+nodes = routes_handler3.nodes
 print("Chargement des nodes terminé : {:d}".format(len(nodes)))
 
-from shapely.geometry import LineString, MultiLineString
-from shapely.ops import cascaded_union
 
+#===============================================
 print("Construction des géometries des ways")
 for w in ways:
     w_nodes = []
@@ -294,6 +147,7 @@ for w in ways:
     w["geom_raw"] = w_nodes
     w["geom"] = LineString(w_nodes)
 
+#===============================================
 print("Construction des géometries des relations")
 for r in routes:
     r_ways = []
@@ -308,16 +162,69 @@ for r in routes:
     r["geom"] = MultiLineString(r_ways_geom)
 
 
+#===============================================
 print("Chargement de la carte")
 import folium
 from folium.plugins import MarkerCluster
 from IPython.display import Image
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
-m = folium.Map(location=[5.6064530999999995,-0.1773089], zoom_start=12, tiles='Cartodb Positron')
+def show_date_on_image(image_path, date_to_display):
+    print("modification de l'image")
+    img = Image.open(image_path)
+    draw = ImageDraw.Draw(img)
+
+    text_offset = [img.size[0] - 200, img.size[1] - 50]
+    text_border = 4
+    text_length = 119
+    text_size = 20
+    draw.rectangle([
+        text_offset[0] - text_border,
+        text_offset[1] - text_border,
+        text_offset[0] + text_border + text_length,
+        text_offset[1] + text_border + text_size
+        ], fill='#858687')
+
+    font = ImageFont.truetype('Pillow/Tests/fonts/LiberationMono-Bold.ttf', text_size)
+    draw.text((text_offset[0], text_offset[1]), date_to_display.strftime('%Y-%m-%d'),'#000000',font=font)
+    img.save(image_path)
+
+attributions = "cartodb | OSM"
+tiles = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
+m = folium.Map(location=[5.6204,-0.2125], zoom_start=12, attr=attributions, tiles=tiles, png_enabled=True)
+
+img_tmp_dir = './images'
+if not os.path.exists(img_tmp_dir):
+    os.makedirs(img_tmp_dir)
+
+start_date = datetime.datetime.strptime('2017-07-15', '%Y-%m-%d')
+end_date = datetime.datetime.strptime('2017-09-06', '%Y-%m-%d')
+delta_days = 5
+date_cursor = start_date
 for r in routes:
-    folium.PolyLine(r["geom_raw"], color="red", weight=2.5, opacity=1).add_to(m)
+    r["displayed"] = False
+while date_cursor <= end_date:
+    for r in routes:
+        if not r["displayed"] and r["creation_date"] < pd.to_datetime(date_cursor):
+            folium.PolyLine(r["geom_raw"], color="red", weight=1.5, opacity=1).add_to(m)
+            r["displayed"] = True
+    print("Enregistrement de la carte pour la date du {}".format(date_cursor.strftime('%Y-%m-%d')))
+    image_path = os.path.join(img_tmp_dir, "image_{}.png".format(date_cursor.strftime('%Y-%m-%d')))
+    with open(image_path, "wb")  as image_file:
+        m._png_image = None #on réinitialise le PNG
+        image_file.write(m._to_png())
+    show_date_on_image(image_path, date_cursor)
+    date_cursor = date_cursor + datetime.timedelta(days=delta_days)
 
-print("Enregistrement de la carte")
-from IPython.display import Image
-with open("./image.png", "wb")  as image_file:
-    image_file.write(m._to_png())
+#===============================================
+print("Création du GIF")
+import imageio
+
+file_names = sorted((os.path.join(img_tmp_dir, fn) for fn in os.listdir(img_tmp_dir) if fn.endswith('.png')))
+
+with imageio.get_writer('final.gif', mode='I', duration=0.4) as writer:
+    for filename in file_names:
+        image = imageio.imread(filename)
+        writer.append_data(image)
---
+++ AccraMobile3/ghana_transit_to_gif_handlers.py
@@ -0,0 +1,132 @@
+# coding: utf-8
+
+import osmium
+import datetime
+import shapely.wkb as wkblib
+
+wkbfab = osmium.geom.WKBFactory()
+
+
+class StopsHandler(osmium.SimpleHandler):
+    def __init__(self):
+        osmium.SimpleHandler.__init__(self)
+        self.start_date = datetime.datetime.strptime("01/07/2017", "%d/%m/%Y").replace(tzinfo=datetime.timezone.utc)
+        self.stops = {}
+
+    def node(self, n):
+        #on prend public_transport = platform et highway=bus_stop
+
+        if n.timestamp < self.start_date:
+            pass
+        elif ('public_transport' in n.tags and n.tags['public_transport'] == 'platform') and \
+             ('highway' in n.tags and n.tags['highway'] == 'bus_stop') :
+            wkb = wkbfab.create_point(n.location)
+            point = wkblib.loads(wkb, hex=True)
+            name = ""
+            public_transport = ""
+            highway = ""
+            if "name" in n.tags:
+                name = n.tags["name"]
+            if "public_transport" in n.tags:
+                public_transport = n.tags["public_transport"]
+            if "highway" in n.tags:
+                highway = n.tags["highway"]
+
+            if not n.id in self.stops:
+                self.stops[n.id] = {}
+                self.stops[n.id]["version"] = []
+                self.stops[n.id]["date"] = []
+                self.stops[n.id]["name"] = []
+                self.stops[n.id]["geometry"] = []
+            self.stops[n.id]["version"].append(n.version)
+            self.stops[n.id]["date"].append(n.timestamp)
+            self.stops[n.id]["name"].append(name)
+            self.stops[n.id]["geometry"].append(point)
+
+
+class RelationHandler(osmium.SimpleHandler):
+    def __init__(self):
+        osmium.SimpleHandler.__init__(self)
+        self.start_date = datetime.datetime.strptime("01/07/2017", "%d/%m/%Y").replace(tzinfo=datetime.timezone.utc)
+        self.routes = {}
+
+    def relation(self, r):
+        #on prend type=route et route=bus
+        if r.timestamp < self.start_date:
+            pass
+        elif ('type' in r.tags and r.tags['type'] == 'route') and ('route' in r.tags and r.tags['route'] == 'bus') :
+            name = ""
+            ref = ""
+            r_type = ""
+            route = ""
+            if "ref" in r.tags:
+                ref = r.tags["ref"]
+            if "name" in r.tags:
+                name = r.tags["name"]
+            ways = []
+            for rm in r.members:
+                if not rm.type == 'w': continue
+                ways.append(rm.ref)
+
+            if not r.id in self.routes:
+                self.routes[r.id] = {}
+                self.routes[r.id]["version"] = []
+                self.routes[r.id]["date"] = []
+                self.routes[r.id]["ref"] = []
+                self.routes[r.id]["name"] = []
+                self.routes[r.id]["ways"] = []
+
+            self.routes[r.id]["version"].append(r.version)
+            self.routes[r.id]["date"].append(r.timestamp)
+            self.routes[r.id]["ref"].append(ref)
+            self.routes[r.id]["name"].append(name)
+            #ways est un tableau qui contient, pour chaque version, un tableau de ways
+            self.routes[r.id]["ways"].append(ways)
+
+
+class WayHandler(osmium.SimpleHandler):
+    def __init__(self, requested_ways):
+        osmium.SimpleHandler.__init__(self)
+        self.requested_ways = requested_ways
+        self.ways = {}
+
+    def way(self, w):
+        if w.id in self.requested_ways:
+            if not w.id in self.ways:
+                self.ways[w.id] = {}
+                self.ways[w.id]["object_id"] = w.id
+                self.ways[w.id]["version"] = ""
+                self.ways[w.id]["date"] = ""
+                self.ways[w.id]["nodes_ref"] = ""
+            if not self.ways[w.id]["version"] or self.ways[w.id]["version"] < w.version:
+                self.ways[w.id]["version"] = w.version
+                self.ways[w.id]["date"] = w.timestamp
+                self.ways[w.id]["nodes_ref"] = [n.ref for n in w.nodes]
+
+
+class NodeHandler(osmium.SimpleHandler):
+    def __init__(self, requested_nodes):
+        osmium.SimpleHandler.__init__(self)
+        self.requested_nodes = requested_nodes
+        self.nodes = {}
+
+    def node(self, n):
+        if n.id in self.requested_nodes:
+            if not n.id in self.nodes:
+                self.nodes[n.id] = {}
+                self.nodes[n.id]["object_id"] = n.id
+                self.nodes[n.id]["version"] = ""
+                self.nodes[n.id]["lat"] = ""
+                self.nodes[n.id]["lon"] = ""
+                self.nodes[n.id]["valid"] = ""
+            if not self.nodes[n.id]["version"] or self.nodes[n.id]["version"] < n.version:
+                self.nodes[n.id]["version"] = n.version
+                try:
+                    if n.location.valid:
+                        self.nodes[n.id]["lat"] = n.location.lat
+                        self.nodes[n.id]["lon"] = n.location.lon
+                    else:
+                        print("point invalide : {:d}".format(n.id))
+                except Exception as e:
+                    print("exception sur le node {:d}".format(self.nodes[n.id]["object_id"]))
+                    #raise
